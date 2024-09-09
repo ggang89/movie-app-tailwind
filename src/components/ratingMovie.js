820aab667b47e125a666158f4106aa3e103593c3
ggang89 <ggang89@gmail.com>
2024-09-09 23:52:11 +0900

    평점순 영화 컴포넌트

---
+++ src/components/ratingMovie.js
@@ -0,0 +1,44 @@
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+export default function RatingMovies({
+  id,
+  title,
+  rating,
+  runtime,
+  genres,
+  img,
+}) {
+  return (
+    <>
+      <img className="relative left-12 bottom-10" src={img} alt={title} />
+      <p className="italic text-center font-bold text-2xl hover:underline ">
+        <Link to={`/movie/${id}`}>{title}</Link>
+      </p>
+      <br />
+      <p className="text-center">
+        rating: ⭐{rating} / 10 &emsp;&emsp;runtime: ⏳ {runtime} min
+      </p>
+
+      <p className="pl-8 flex flex-wrap">
+        Genres:
+        {genres.map((g) => (
+          <span
+            className="italic bg-red-700 rounded-xl m-0.5 pr-1 align-middle"
+            key={g}
+          >
+            &nbsp; {g}&nbsp;
+          </span>
+        ))}
+      </p>
+    </>
+  );
+}
+RatingMovies.propTypes = {
+  id: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+  runtime: PropTypes.number.isRequired,
+  genres: PropTypes.array.isRequired,
+  img: PropTypes.string.isRequired,
+};
--- src/routes/rate.jsx
+++ src/routes/rate.jsx
@@ -1,9 +1,49 @@
+
+import { useEffect, useState } from "react";
+import RatingMovies from "../components/ratingMovie";
+
 export default function Rate() {
-  
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const response = await fetch(
+      "https://yts.mx/api/v2/list_movies.json?sort_by=rating&&limit=20"
+    );
+    const json = await response.json();
+    console.log(json.data.movies);
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
   return (
-    <>
-    
-    <h1>평점순 영화</h1>
-    </>
-  )
+    <div className=" h-vh flex justify-center bg-black text-white p-3 ">
+      {loading ? (
+        <div className="h-dvh flex items-center bg-black text-center text-4xl font-bold">
+          <p className="text-center text-4xl font-bold">Loading...🐧...</p>
+        </div>
+      ) : (
+        <>
+          <div className="  flex flex-wrap justify-center">
+            {movies.map((m) => (
+              <div
+                key={m.id}
+                className="shadow-lg shadow-gray-400 w-1/4 p-3 m-10 "
+              >
+                <RatingMovies
+                  id={m.id}
+                  title={m.title}
+                  rating={m.rating}
+                  runtime={m.runtime}
+                  genres={m.genres}
+                  img={m.medium_cover_image}
+                />
+              </div>
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
